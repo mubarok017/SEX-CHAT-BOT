bd13dd908e4a07c8fc574d998447a35cd64c9cc5
mubarok017 <suhanahmed1270@gmail.com>
2026-08-24 23:22:19 +0600

    Update antiProtect.js

--- Script/events/antiProtect.js
+++ Script/events/antiProtect.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 module.exports.config = {
   name: "antiProtect",
   version: "1.0.0",
-  credits: "SHAHADAT SAHU",
+  credits: "𝚂𝚄ℍ𝙰𝙽 𝙲ℍ𝙰𝚃 𝙱𝙾𝚃",
   description: "Protect group name and photo",
   eventType: ["log:thread-name", "log:thread-icon"],
   cooldowns: 3

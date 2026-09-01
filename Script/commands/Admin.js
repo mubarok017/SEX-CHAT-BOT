0f8c9f49b9cb0c4578928474a4a3122fadf4cbb8
mubarok017 <suhanahmed1270@gmail.com>
2026-09-01 13:45:55 +0600

    Update Imgur link in Admin.js

--- Script/commands/Admin.js
+++ Script/commands/Admin.js
@@ -49,7 +49,7 @@ module.exports.run = async function({ api, event }) {
  attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
  }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/owner.jpg"));
 
- return request("https://imgur.com/a/v3VqsZf") //এখানে আপনার ছবির Imgur link বসাবেন✅
+ return request("https://i.imgur.com/KUc4f50.jpeg") //এখানে আপনার ছবির Imgur link বসাবেন✅
  .pipe(fs.createWriteStream(__dirname + '/cache/owner.jpg'))
  .on('close', () => callback());
 };
